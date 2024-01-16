7f0c90ca0d5b0971fd89b008b1a8d6303c460bd0
Prince Sharma <109868643+prince1184@users.noreply.github.com>
2024-01-16 21:33:36 +0530

    Add files via upload

---
+++ DesktopJoinGroup.js
@@ -0,0 +1,305 @@
+import styles from "./DesktopJoinGroup.module.css";
+
+const DesktopJoinGroup = () => {
+  return (
+    <div className={styles.desktopJoinGroup}>
+      <div className={styles.groupParent}>
+        <div className={styles.rectangleParent}>
+          <div className={styles.rectangle} />
+          <img className={styles.wholeIcon} alt="" src="/whole.svg" />
+          <div className={styles.rectangleGroup}>
+            <div className={styles.rectangle1} />
+            <img className={styles.groupChild} alt="" src="/group-28.svg" />
+            <div className={styles.searchForYour}>
+              Search for your favorite groups in ATG
+            </div>
+          </div>
+          <div className={styles.createAccountItsContainer}>
+            <span className={styles.createAccount}>{`Create account. `}</span>
+            <b className={styles.itsFree}>It’s free!</b>
+          </div>
+          <img
+            className={styles.baselineArrowDropDown24pxIcon}
+            alt=""
+            src="/baselinearrow-drop-down24px.svg"
+          />
+        </div>
+        <div className={styles.line} />
+        <div className={styles.groupContainer}>
+          <img className={styles.groupItem} alt="" src="/group-58.svg" />
+          <div className={styles.rectangleContainer}>
+            <div className={styles.groupInner} />
+            <div className={styles.writeAPost}>Write a Post</div>
+            <img
+              className={styles.baselineEdit24pxIcon}
+              alt=""
+              src="/baselineedit24px.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.line1} />
+        <div className={styles.lineDiv} />
+        <div className={styles.allPosts32Parent}>
+          <div className={styles.allPosts32}>All Posts(32)</div>
+          <div className={styles.article}>Article</div>
+          <div className={styles.event}>Event</div>
+          <div className={styles.education}>Education</div>
+          <div className={styles.job}>Job</div>
+        </div>
+        <div className={styles.groupDiv}>
+          <div className={styles.rectangleDiv} />
+          <img
+            className={styles.baselineMoreVert24pxIcon}
+            alt=""
+            src="/baselinemore-vert24px@2x.png"
+          />
+          <div className={styles.article1}>✍️ Article</div>
+          <div className={styles.whatIfFamous}>
+            What if famous brands had regular fonts? Meet RegulaBrands!
+          </div>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="/rectangle-5@2x.png"
+          />
+          <div className={styles.iveWorkedIn}>
+            I’ve worked in UX for the better part of a decade. From now on, I
+            plan to rei…
+          </div>
+          <img
+            className={styles.iconactionvisibility24px}
+            alt=""
+            src="/iconactionvisibility-24px@2x.png"
+          />
+          <div className={styles.kViews}>1.4k views</div>
+          <img
+            className={styles.groupChild1}
+            alt=""
+            src="/rectangle-3@2x.png"
+          />
+          <div className={styles.sarthakKamra}>Sarthak Kamra</div>
+        </div>
+        <div className={styles.rectangleParent1}>
+          <div className={styles.rectangleDiv} />
+          <img
+            className={styles.baselineMoreVert24pxIcon}
+            alt=""
+            src="/baselinemore-vert24px@2x.png"
+          />
+          <div className={styles.article1}>🔬️ Education</div>
+          <div className={styles.taxBenefitsFor}>
+            Tax Benefits for Investment under National Pension Scheme launched
+            by Government
+          </div>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="/rectangle-51@2x.png"
+          />
+          <div className={styles.iveWorkedIn}>
+            I’ve worked in UX for the better part of a decade. From now on, I
+            plan to rei…
+          </div>
+          <img
+            className={styles.iconactionvisibility24px}
+            alt=""
+            src="/iconactionvisibility-24px@2x.png"
+          />
+          <div className={styles.kViews}>1.4k views</div>
+          <img
+            className={styles.groupChild1}
+            alt=""
+            src="/rectangle-31@2x.png"
+          />
+          <div className={styles.sarthakKamra}>Sarah West</div>
+        </div>
+        <div className={styles.groupParent1}>
+          <div className={styles.rectangleParent2}>
+            <div className={styles.groupChild5} />
+            <img
+              className={styles.outlineMyLocation24pxIcon}
+              alt=""
+              src="/outlinemy-location24px.svg"
+            />
+            <div className={styles.useCurrentLocation}>
+              Use Current Location
+            </div>
+          </div>
+          <div className={styles.or}>or</div>
+          <div className={styles.lineParent}>
+            <div className={styles.groupChild6} />
+            <div className={styles.enterLocationManually}>
+              Enter location manually
+            </div>
+            <img
+              className={styles.outlineSearch24pxIcon}
+              alt=""
+              src="/outlinesearch24px.svg"
+            />
+          </div>
+          <div className={styles.locationParent}>
+            <div className={styles.location}>Location</div>
+            <img
+              className={styles.outlineLocationOn24px2Icon}
+              alt=""
+              src="/outlinelocation-on24px-2.svg"
+            />
+          </div>
+          <div className={styles.yourLocationWillHelpUsSerParent}>
+            <div className={styles.yourLocationWill}>
+              Your location will help us serve better and extend a personalised
+              experience.
+            </div>
+            <img
+              className={styles.baselineErrorOutline24pxIcon}
+              alt=""
+              src="/baselineerror-outline24px.svg"
+            />
+          </div>
+        </div>
+        <div className={styles.connectParent}>
+          <div className={styles.location}>connect</div>
+          <img
+            className={styles.outlineLocationOn24px2Icon}
+            alt=""
+            src="/outlinegroup24px.svg"
+          />
+        </div>
+        <div className={styles.rectangleParent3}>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-6@2x.png"
+          />
+          <div className={styles.sarthakKamra1}>Sarthak Kamra</div>
+          <div className={styles.groupChild8} />
+          <div className={styles.connect1}>Connect</div>
+        </div>
+        <div className={styles.rectangleParent4}>
+          <div className={styles.groupChild8} />
+          <div className={styles.connect2}>Connect</div>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-61@2x.png"
+          />
+          <div className={styles.johnDoe}>Sarah West</div>
+        </div>
+        <div className={styles.rectangleParent5}>
+          <div className={styles.groupChild11} />
+          <div className={styles.connected}>Connected</div>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-62@2x.png"
+          />
+          <div className={styles.joyLawson}>Joy Lawson</div>
+        </div>
+        <div className={styles.rectangleParent6}>
+          <div className={styles.groupChild8} />
+          <div className={styles.connect2}>Connect</div>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-63@2x.png"
+          />
+          <div className={styles.johnDoe}>John Doe</div>
+        </div>
+        <div className={styles.rectangleParent7}>
+          <div className={styles.groupChild8} />
+          <div className={styles.connect2}>Connect</div>
+          <img
+            className={styles.groupChild7}
+            alt=""
+            src="/rectangle-64@2x.png"
+          />
+          <div className={styles.johnDoe}>Siddharth Goyal</div>
+        </div>
+        <div className={styles.seeMore}>See More...</div>
+        <div className={styles.rectangleParent8}>
+          <div className={styles.groupChild17} />
+          <div className={styles.writeAPost}>Leave Group</div>
+          <img
+            className={styles.baselineEdit24pxIcon}
+            alt=""
+            src="/outlineexit-to-app24px.svg"
+          />
+        </div>
+        <img className={styles.subtractIcon} alt="" src="/subtract.svg" />
+      </div>
+      <div className={styles.rectangle2} />
+      <div className={styles.groupParent2}>
+        <div className={styles.rectangleParent9}>
+          <div className={styles.rectangle3} />
+          <img className={styles.groupIcon} alt="" src="/group-3@2x.png" />
+          <div
+            className={styles.bySigningUp}
+          >{`By signing up, you agree to our Terms & conditions, Privacy policy`}</div>
+          <div className={styles.groupChild18} />
+          <div
+            className={styles.letsLearnShare}
+          >{`Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼`}</div>
+          <div className={styles.alreadyHaveAnContainer}>
+            <span>{`Already have an account? `}</span>
+            <span className={styles.signIn}>Sign In</span>
+          </div>
+          <div className={styles.createAccountParent}>
+            <b className={styles.createAccount1}>Create Account</b>
+            <div className={styles.rectangleParent10}>
+              <div className={styles.groupChild19} />
+              <div className={styles.group}>
+                <img className={styles.searchIcon} alt="" src="/search.svg" />
+                <div className={styles.signUpWith}>Sign up with Google</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent11}>
+              <div className={styles.groupChild19} />
+              <div className={styles.signUpWithFacebookParent}>
+                <div className={styles.signUpWith}>Sign up with Facebook</div>
+                <img
+                  className={styles.fLogoRgbBlue1024Icon}
+                  alt=""
+                  src="/f-logo-rgbblue-1024@2x.png"
+                />
+              </div>
+            </div>
+            <div className={styles.groupParent3}>
+              <div className={styles.rectangleParent12}>
+                <div className={styles.groupChild21} />
+                <div className={styles.createAccount2}>Create Account</div>
+              </div>
+              <div className={styles.groupParent4}>
+                <div className={styles.rectangleParent13}>
+                  <div className={styles.groupChild22} />
+                  <div className={styles.email}>Email</div>
+                </div>
+                <div className={styles.rectangleParent14}>
+                  <div className={styles.groupChild23} />
+                  <div className={styles.email}>Confirm Password</div>
+                </div>
+                <div className={styles.rectangleParent15}>
+                  <div className={styles.groupChild24} />
+                  <div className={styles.email}>First Name</div>
+                  <div className={styles.lastName}>Last Name</div>
+                  <div className={styles.line2} />
+                </div>
+                <div className={styles.rectangleParent16}>
+                  <div className={styles.groupChild22} />
+                  <div className={styles.email}>Password</div>
+                  <img
+                    className={styles.groupChild26}
+                    alt=""
+                    src="/group-31@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img className={styles.groupChild27} alt="" src="/group-32.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default DesktopJoinGroup;
